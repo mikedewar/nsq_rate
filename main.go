29c97d7e4f0ad80606022eeb086a52fd90f61833
Mike Dewar <mikedewar@gmail.com>
2015-03-17 19:26:29 +0000

    converges after a while. probably too quickly

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ import (
 
 var (
 	n                = flag.Int("n", 10, "number of samples to collect before updating")
-	β                = flag.Float64("beta", 0, "prior rate")
-	α                = flag.Float64("alpha", 0, "prior shape")
+	βprior           = flag.Float64("beta", 0, "prior rate")
+	αprior           = flag.Float64("alpha", 0, "prior shape")
 	lookupdHTTPAddrs = app.StringArray{}
 	maxInFlight      = flag.Int("max-in-flight", 10, "max number of messages to allow in flight")
 )
@@ -35,45 +35,60 @@ type RateEstimator struct {
 	α          float64
 	β          float64
 	eventTimes chan time.Time
+	timeChan   chan time.Time
+	topic      string
 }
 
-func NewRateEstimator(α, β float64, n int) RateEstimator {
-	return RateEstimator{
+func NewRateEstimator(α, β float64, n int, topic string) *RateEstimator {
+	re := &RateEstimator{
 		α:          α,
 		β:          β,
 		eventTimes: make(chan time.Time, n),
+		timeChan:   make(chan time.Time),
+		topic:      topic,
 	}
+	go re.Start()
+	return re
 }
 
-func (re RateEstimator) HandleMessage(m *nsq.Message) error {
-	t := time.Now()
-	select {
-	case re.eventTimes <- t:
-	default:
-		<-re.eventTimes
-		re.eventTimes <- t
-		re.Update()
-	}
+func (re *RateEstimator) HandleMessage(m *nsq.Message) error {
+	re.timeChan <- time.Now()
 	return nil
 }
 
-func (re RateEstimator) Update() {
+func (re *RateEstimator) Start() {
+	for {
+		t := <-re.timeChan
+		select {
+		case re.eventTimes <- t:
+		default:
+			re.Update()
+			re.eventTimes <- t
+		}
+	}
+}
+
+func (re *RateEstimator) Update() {
 	var Δhat float64
 	N := len(re.eventTimes)
 	ti := <-re.eventTimes
 	n := 1
 	for t := range re.eventTimes {
-		Δ := float64(t.Sub(ti))
+		Δ := t.Sub(ti).Seconds()
 		Δhat += Δ / float64(n)
 		ti = t
 		n++
-		if n == N-1 {
+		if n == N {
 			break
 		}
 	}
 	re.α += float64(n)
 	re.β += float64(n) * Δhat
-	log.Println(α, β)
+	/*
+		if re.topic == "abtest" {
+			log.Println(re.topic, "  α:", re.α, "  β:", re.β, "  mean:", re.α/re.β, "  variance:", re.α/math.Pow(re.β, 2))
+		}
+	*/
 }
 
 func main() {
@@ -89,7 +104,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		u.Scheme = "http"
+		u.Path = "/topics"
+		log.Println(u.String())
 		resp, err := http.Get(u.String())
 		if err != nil {
 			log.Println(err)
@@ -113,9 +129,9 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		handler := NewRateEstimator(*α, *β, *n)
+		handler := NewRateEstimator(*αprior, *βprior, *n, topic)
 		consumer.AddHandler(handler)
-		rateEstimators[topic] = &handler
+		rateEstimators[topic] = handler
 		err = consumer.ConnectToNSQLookupds(lookupdHTTPAddrs)
 		if err != nil {
 			log.Fatal(err)
